e0a5e078335d77474795f3a502301fce18b80dd7
Victor R <victor.rocha@mercadolivre.com>
2019-09-08 17:43:28 -0300

    added main view holder test

---
+++ app/src/test/java/com/stonetree/imagebucket/main/view/adapter/MainViewHolderTest.kt
@@ -0,0 +1,26 @@
+package com.stonetree.imagebucket.main.view.adapter
+
+import android.net.Uri
+import com.stonetree.imagebucket.main.model.MainModel
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.mock
+
+class MainViewHolderTest {
+
+    private val holder = mock(MainViewHolder::class.java)
+
+    @Test
+    fun test_onBind_ShouldReturnSame() {
+        val uri = mock(Uri::class.java)
+        val model = MainModel(uri, "mName")
+
+        holder.onBind(model)
+
+        verify(holder).onBind(model)
+
+        assertEquals(uri, model.imageUrl)
+        assertEquals("mName", model.imageName)
+    }
+}
