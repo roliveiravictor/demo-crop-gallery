c5f4f24063b6cbd59329df50a9265e86d81979b0
Victor R <victor.rocha@mercadolivre.com>
2019-09-10 09:48:17 -0300

    removed reflection extension

--- app/src/androidTest/java/com/stonetree/imagebucket/main/viewmodel/MainViewModelTest.kt
+++ app/src/androidTest/java/com/stonetree/imagebucket/main/viewmodel/MainViewModelTest.kt
@@ -2,9 +2,7 @@ package com.stonetree.imagebucket.main.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.stonetree.imagebucket.accessField
 import com.stonetree.imagebucket.main.resources.repository.MainRepositoryImpl
-import junit.framework.TestCase.assertNotNull
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.any
 import org.hamcrest.MatcherAssert.assertThat
@@ -15,15 +13,6 @@ import org.junit.runner.RunWith
 class MainViewModelTest {
 
     private val vm: MainViewModel = MainViewModel(MainRepositoryImpl())
-
-    @Test
-    fun test_viewModelRepository_shouldReturnNotEmpty() {
-        val repository = vm
-            .accessField("repository")
-                as MainRepositoryImpl
-
-        assertNotNull(repository)
-    }
 
     @Test
     fun test_viewModelImages_shouldReturnMutableLiveData() {
