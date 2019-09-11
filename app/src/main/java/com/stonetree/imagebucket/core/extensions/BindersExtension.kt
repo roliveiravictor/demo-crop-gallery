226ec9fc063cd2c43b0a60aa778a959876c8e2c4
Victor R <victor.rocha@mercadolivre.com>
2019-09-11 18:58:20 -0300

    tests renewal

--- app/src/main/java/com/stonetree/imagebucket/core/extensions/BindersExtension.kt
+++ app/src/main/java/com/stonetree/imagebucket/core/extensions/BindersExtension.kt
@@ -1,23 +1,25 @@
 package com.stonetree.imagebucket.core.extensions
 
 import android.view.View
+import android.view.View.VISIBLE
+import android.view.View.GONE
 import androidx.databinding.BindingAdapter
 import com.stonetree.imagebucket.core.network.NetworkState
+import com.stonetree.imagebucket.core.network.NetworkState.Companion.LOADING
 import com.stonetree.imagebucket.core.network.Status
 
 @BindingAdapter("isGone")
 fun bindIsGone(view: View, network: NetworkState?) {
     when (network) {
-        NetworkState.LOADING -> view.visibility = View.VISIBLE
-        else -> view.visibility = View.GONE
+        LOADING -> view.visibility = VISIBLE
+        else -> view.visibility = GONE
     }
 }
 
 @BindingAdapter("isIdle")
 fun bindIsIdle(view: View, network: NetworkState?) {
     when (network?.status) {
-        Status.SUCCESS -> view.visibility = View.VISIBLE
-        Status.FAILED -> view.visibility = View.VISIBLE
-        else -> view.visibility = View.GONE
+        Status.RUNNING -> view.visibility = GONE
+        else -> view.visibility = VISIBLE
     }
 }
