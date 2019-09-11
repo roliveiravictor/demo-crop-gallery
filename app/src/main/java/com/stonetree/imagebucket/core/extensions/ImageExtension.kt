0a60d79f229ddc017529c2a339b7a2bfde0b8ba4
Victor R <victor.rocha@mercadolivre.com>
2019-09-11 19:48:32 -0300

    added image extensions test

--- app/src/main/java/com/stonetree/imagebucket/core/extensions/ImageExtension.kt
+++ app/src/main/java/com/stonetree/imagebucket/core/extensions/ImageExtension.kt
@@ -4,24 +4,23 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import com.stonetree.imagebucket.core.constants.Constants
+import com.stonetree.imagebucket.core.constants.Constants.INTENT_EXTRA_DATA
 import java.io.File
 import java.io.FileOutputStream
 
 fun Intent.getCachedImage(context: Context): File {
-    val image = createTemporaryFile(context)
-    val output = FileOutputStream(image)
-    (this?.extras?.get(Constants.INTENT_EXTRA_DATA) as Bitmap).let { bitmap ->
-        bitmap.compress(Bitmap.CompressFormat.JPEG, Constants.BITMAP_QUALITY, output)
-        output.flush()
-        output.close()
-    }
-    return image
-}
-
-fun createTemporaryFile(context: Context): File {
-    return File.createTempFile(
+    val image = File.createTempFile(
         "img_",
         ".jpg",
         context.cacheDir
     )
+    val output = FileOutputStream(image)
+
+    (extras?.get(INTENT_EXTRA_DATA) as? Bitmap).let { bitmap ->
+        bitmap?.compress(Bitmap.CompressFormat.JPEG, Constants.BITMAP_QUALITY, output)
+        output.flush()
+        output.close()
+    }
+
+    return image
 }
