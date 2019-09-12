286b919778bdaee2d30fab73ba12385d74f8baad
Victor R <victor.rocha@mercadolivre.com>
2019-09-11 23:53:02 -0300

    added exclusion rule for files

--- app/src/androidTest/java/com/stonetree/imagebucket/core/extensions/core/extensions/BindersExtensionTest.kt
+++ app/src/androidTest/java/com/stonetree/imagebucket/core/extensions/core/extensions/BindersExtensionTest.kt
@@ -18,7 +18,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class BinderExtensionTest {
+class BindersExtensionTest {
 
     private val context = InstrumentationRegistry.getInstrumentation().targetContext
 
