ddd16ddb203330482ae5ffabeee71da1acb2ce63
Victor R <victor.rocha@mercadolivre.com>
2019-09-18 18:29:24 -0300

    on hold repository test

--- app/src/test/java/com/stonetree/imagebucket/main/repository/GalleryRepositoryTest.kt
+++ app/src/test/java/com/stonetree/imagebucket/main/repository/GalleryRepositoryTest.kt
@@ -1,13 +1,38 @@
 package com.stonetree.imagebucket.main.repository
 
+import com.stonetree.imagebucket.main.viewmodel.GalleryViewModel
+import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito.mock
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.startKoin
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.inject
+import org.koin.test.mock.declareMock
+import org.mockito.Mockito.verify
 
-class GalleryRepositoryTest {
+class GalleryRepositoryTest: AutoCloseKoinTest() {
 
-    private val repository = mock(GalleryRepositoryImpl::class.java)
+    private val module = module {
+        named("repository").apply {
+            single<GalleryRepository>(this) { GalleryRepositoryImpl() }
+            single { GalleryViewModel(get(this.javaClass)) }
+        }
+    }
+
+    private val repository: GalleryRepositoryImpl by inject()
+    private val vm: GalleryViewModel by inject()
+
+    @Before
+    fun setup() {
+        startKoin { loadKoinModules(module) }
+        declareMock<GalleryRepositoryImpl>()
+    }
 
     @Test
     fun uploadImage_withEmptyUri_shouldDoNothing() {
+        vm.delete("")
+        verify(repository).delete("")
     }
 }
