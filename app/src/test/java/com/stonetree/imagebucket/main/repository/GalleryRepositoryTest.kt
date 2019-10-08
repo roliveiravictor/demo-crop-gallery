ab08607ef986a72b4dc15efd555588ecfcc2d903
Victor R <victor.rocha@mercadolivre.com>
2019-10-08 18:37:29 -0300

    added get all images test

--- app/src/test/java/com/stonetree/imagebucket/main/repository/GalleryRepositoryTest.kt
+++ app/src/test/java/com/stonetree/imagebucket/main/repository/GalleryRepositoryTest.kt
@@ -1,9 +1,12 @@
 package com.stonetree.imagebucket.main.repository
 
 import android.net.Uri
+import com.google.android.gms.tasks.Task
 import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.HttpsCallableResult
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.UploadTask
+import com.stonetree.imagebucket.core.constants.CloudFunctions.GET_IMAGES
 import com.stonetree.imagebucket.core.constants.Constants.FIREBASE_IMAGES_PATH
 import com.stonetree.imagebucket.core.network.NetworkState.Companion.LOADING
 import com.stonetree.imagebucket.gallery.repository.GalleryRepositoryImpl
@@ -12,11 +15,14 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.test.AutoCloseKoinTest
-import org.mockito.Mockito.*
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.RETURNS_DEEP_STUBS
+import org.mockito.Mockito.`when`
 import org.robolectric.RobolectricTestRunner
 
+@Suppress("UNCHECKED_CAST")
 @RunWith(RobolectricTestRunner::class)
-class GalleryRepositoryTest {
+class GalleryRepositoryTest : AutoCloseKoinTest() {
 
     /**
      * Warning:
@@ -36,18 +42,32 @@ class GalleryRepositoryTest {
     }
 
     @Test
-    fun uploadImage_withEmptyUri_shouldDoNothing() {
+    fun uploadImage_withEmptyUri_shouldEnableLoadingStatus() {
         val hash = FIREBASE_IMAGES_PATH
         val uri = Uri.parse(hash)
 
         `when`(
-            storage.
-                reference
+            storage
+                .reference
                 .child(hash)
                 .putFile(uri)
         ).thenReturn(mock(UploadTask::class.java))
 
         repository.uploadImage(uri, hash)
+
+        assertEquals(LOADING, repository.getState().value)
+    }
+
+    @Test
+    fun getAllImages_shouldEnableLoadingStatus() {
+        repository.getAllImages()
+
+        val task = mock(Task::class.java)
+        `when`(
+            functions
+                .getHttpsCallable(GET_IMAGES)
+                .call()
+        ).thenReturn(task as Task<HttpsCallableResult>)
 
         assertEquals(LOADING, repository.getState().value)
     }
