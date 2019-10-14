f9e3a4391ebd53bc8d0b4ecf000079fff6efdbeb
Victor R <victor.rocha@mercadolivre.com>
2019-10-14 10:58:00 -0300

    fixed request intent

--- app/src/androidTest/java/com/stonetree/imagebucket/gallery/view/GalleryActivityTest.kt
+++ app/src/androidTest/java/com/stonetree/imagebucket/gallery/view/GalleryActivityTest.kt
@@ -2,25 +2,21 @@ package com.stonetree.imagebucket.gallery.view
 
 import android.app.Instrumentation
 import android.content.Intent
-import android.net.Uri
 import android.view.View
-import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
 import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.airbnb.lottie.LottieAnimationView
 import com.stonetree.imagebucket.R
-import com.theartofdev.edmodo.cropper.CropImage
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.`when`
-import org.mockito.Mockito.mock
 
 @RunWith(AndroidJUnit4::class)
 class MainViewTest {
@@ -54,11 +50,7 @@ class MainViewTest {
 
     @Test
     fun camera_shouldReturnRequestPermission() {
-        mock(CropImage.ActivityResult::class.java).apply {
-            `when`(uri).thenReturn(mock(Uri::class.java))
-        }
-
-        toPackage("com.android.camera").apply {
+        toPackage("com.stonetree.imagebucket").apply {
             intending(this).respondWith(dummy)
             onView(withId(R.id.camera)).perform(click())
             intended(this)
